fe3dcebdfdf77ca575f4cc72e2b5b4fedc563bf0
MatthewHink <matthew_hink@yahoo.com>
2018-05-29 09:20:31 -0700

    ups_mib_test cleanup

--- snmp/mibs/ups_mib/ups_mib_test.go
+++ snmp/mibs/ups_mib/ups_mib_test.go
@@ -24,8 +24,6 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 		t.Fatal(err) // Fail the test.
 	}
 
-	t.Logf("securityParameters: %+v", securityParameters)
-
 	// Create a config.
 	config, err := core.NewDeviceConfig(
 		"v3",        // SNMP v3
@@ -37,15 +35,11 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 		t.Fatal(err) // Fail the test.
 	}
 
-	t.Logf("config: %+v", config)
-
 	// Create a client.
 	client, err := core.NewSnmpClient(config)
 	if err != nil {
 		t.Fatal(err) // Fail the test.
 	}
-
-	t.Logf("client: %+v", client)
 
 	// Create SnmpServerBase
 	snmpServer, err := core.NewSnmpServerBase(
@@ -54,16 +48,12 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 	if err != nil {
 		t.Fatal(err) // Fail the test.
 	}
-	t.Logf("snmpServer: %+v", snmpServer)
 
 	// Create the UpsMib and dump it.
-	fmt.Printf("TestUpsMib creating mib\n")
 	testUpsMib, err := NewUpsMib(snmpServer)
 	if err != nil {
 		t.Fatal(err) // Fail the test.
 	}
-	fmt.Printf("TestUpsMib created mib\n")
-	//fmt.Printf("testUpsMib: %+v\n", testUpsMib)
 	testUpsMib.Dump()
 
 	// We should have 19 tables.
@@ -74,13 +64,6 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 
 	// Get the ups identity data from the test MIB.
 	upsIdentity := testUpsMib.UpsIdentityTable.UpsIdentity
-	fmt.Printf("upsIdentity:  %+v\n", upsIdentity)
-	fmt.Printf("Manufacturer:         %v\n", upsIdentity.Manufacturer)
-	fmt.Printf("Model:                %v\n", upsIdentity.Model)
-	fmt.Printf("UpsSoftwareVersion:   %v\n", upsIdentity.UpsSoftwareVersion)
-	fmt.Printf("AgentSoftwareVersion: %v\n", upsIdentity.AgentSoftwareVersion)
-	fmt.Printf("Name:                 %v\n", upsIdentity.Name)
-	fmt.Printf("AttachedDevices:      %v\n", upsIdentity.AttachedDevices)
 
 	// Verify expected ups identity data from the test MIB.
 	if upsIdentity == nil {
@@ -88,48 +71,52 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 	}
 
 	if upsIdentity.Manufacturer != "Eaton Corporation" {
-		t.Fatalf("Expected upsIdentity.Manufacturer [Eaton Corporation], got [%v]", upsIdentity.Manufacturer)
+		t.Fatalf("Expected upsIdentity.Manufacturer [Eaton Corporation], got [%v]",
+			upsIdentity.Manufacturer)
 	}
 
 	if upsIdentity.Model != "PXGMS UPS + EATON 93PM" {
-		t.Fatalf("Expected upsIdentity.Model [PXGMS UPS + EATON 93PM], got [%v]", upsIdentity.Model)
+		t.Fatalf("Expected upsIdentity.Model [PXGMS UPS + EATON 93PM], got [%v]",
+			upsIdentity.Model)
 	}
 
 	if upsIdentity.UpsSoftwareVersion != "INV: 1.44.0000" {
-		t.Fatalf("Expected upsIdentity.UpsSoftwareVersion [INV: 1.44.0000], got [%v]", upsIdentity.UpsSoftwareVersion)
+		t.Fatalf("Expected upsIdentity.UpsSoftwareVersion [INV: 1.44.0000], got [%v]",
+			upsIdentity.UpsSoftwareVersion)
 	}
 
 	if upsIdentity.AgentSoftwareVersion != "2.3.7" {
-		t.Fatalf("Expected upsIdentity.AgentSoftwareVersion [2.3.7], got [%v]", upsIdentity.AgentSoftwareVersion)
+		t.Fatalf("Expected upsIdentity.AgentSoftwareVersion [2.3.7], got [%v]",
+			upsIdentity.AgentSoftwareVersion)
 	}
 
 	if upsIdentity.Name != "ID: EM111UXX06, Msg: 9PL15N0000E40R2" {
-		t.Fatalf("Expected upsIdentity.Name [ID: EM111UXX06, Msg: 9PL15N0000E40R2], got [%v]", upsIdentity.Name)
+		t.Fatalf("Expected upsIdentity.Name [ID: EM111UXX06, Msg: 9PL15N0000E40R2], got [%v]",
+			upsIdentity.Name)
 	}
 
 	if upsIdentity.AttachedDevices != "Attached Devices not set" {
-		t.Fatalf("Expected upsIdentity.AttachedDevices [Attached Devices not set], got [%v]", upsIdentity.AttachedDevices)
+		t.Fatalf("Expected upsIdentity.AttachedDevices [Attached Devices not set], got [%v]",
+			upsIdentity.AttachedDevices)
 	}
 
 	// Call the ups battery table device enumerator.
 	upsBatteryTable := testUpsMib.UpsBatteryTable
-	fmt.Printf("ZZZ (Test): Calling UpsBatteryTable device enumerator (Third try)\n")
-	fmt.Printf("This is the one that works!!!\n")
-	devices, err := upsBatteryTable.SnmpTable.DevEnumerator.DeviceEnumerator(map[string]interface{}{"rack": "my_pet_rack", "board": "my_pet_board"})
-	fmt.Printf("ZZZ (Test): Called UpsBatteryTable device enumerator. devices %+v, err %x\n", devices, err)
+	devices, err := upsBatteryTable.SnmpTable.DevEnumerator.DeviceEnumerator(
+		map[string]interface{}{"rack": "my_pet_rack", "board": "my_pet_board"})
+
 	// Ensure devices and no error.
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(devices) == 0 { // len(nil) is defined as zero in golang.
-		// It's really easy to mess up the enumeration call and call the default,
-		// so test that we don't do that.
-		t.Fatalf("Expected devices, got none.\n")
+	if len(devices) != 7 {
+		t.Fatalf("Expected 7 devices from the UpsBatteryTable, got %d", len(devices))
 	}
 
 	// Enumerate UpsInputTable devices.
 	upsInputTable := testUpsMib.UpsInputTable
-	devices, err = upsInputTable.SnmpTable.DevEnumerator.DeviceEnumerator(map[string]interface{}{"rack": "my_pet_rack", "board": "my_pet_board"})
+	devices, err = upsInputTable.SnmpTable.DevEnumerator.DeviceEnumerator(
+		map[string]interface{}{"rack": "my_pet_rack", "board": "my_pet_board"})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -137,14 +124,17 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 		t.Fatalf("Expected devices, got none.\n")
 	}
 
+	// Ensure devices and no error.
+	if len(devices) != 12 {
+		t.Fatalf("Expected 12 devices from the UpsInputTable, got %d", len(devices))
+	}
+
 	// Enumerate the mib.
 	// Testing for bad parameters is in TestDevices.
-	devices, err = testUpsMib.EnumerateDevices(map[string]interface{}{"rack": "test_rack", "board": "test_board"})
+	devices, err = testUpsMib.EnumerateDevices(
+		map[string]interface{}{"rack": "test_rack", "board": "test_board"})
 	if err != nil {
 		t.Fatal(err)
-	}
-	if len(devices) == 0 {
-		t.Fatalf("Expected devices, got none.\n")
 	}
 	if len(devices) != 40 {
 		t.Fatalf("Expected 40 devices, got %d.\n", len(devices))
@@ -162,8 +152,39 @@ func TestUpsMib(t *testing.T) { // nolint: gocyclo
 			devices[i].Data["column"])
 	}
 
-	fmt.Printf("Dumping full first device data: %v\n", devices[0].Data)
-	fmt.Printf("Dumping second first device data: %v\n", devices[1].Data)
+	// Verify two devices in more detail.
+	if devices[0].Data == nil {
+		t.Fatalf("Expected Data != nil")
+	}
+	if devices[0].Data["table_name"] != "UPS-MIB-UPS-Identity-Table" {
+		t.Fatalf("Expected TableName == [UPS-MIB-UPS-Identity-Table], got [%v]", devices[0].Data["table_name"])
+	}
+	if devices[0].Type != "identity" {
+		t.Fatalf("Expected Type == [identity], got [%v]", devices[0].Type)
+	}
+	if devices[0].Data["info"] != "upsIdentManufacturer" {
+		t.Fatalf("Expected Info == [upsIdentManufacturer], got [%v]", devices[0].Data["info"])
+	}
+	if devices[0].Data["oid"] != ".1.3.6.1.2.1.33.1.1.1.0" {
+		t.Fatalf("Expected oid == [.1.3.6.1.2.1.33.1.1.1.0], got [%v]", devices[0].Data["oid"])
+	}
+
+	// Verify two devices in more detail.
+	if devices[20].Data == nil {
+		t.Fatalf("Expected Data != nil")
+	}
+	if devices[20].Data["table_name"] != "UPS-MIB-UPS-Input-Table" {
+		t.Fatalf("Expected TableName == [UPS-MIB-UPS-Input-Table], got [%v]", devices[20].Data["table_name"])
+	}
+	if devices[20].Type != "power" {
+		t.Fatalf("Expected Type == [power], got [%v]", devices[20].Type)
+	}
+	if devices[20].Data["info"] != "upsInputTruePower1" {
+		t.Fatalf("Expected Info == [upsInputTruePower1], got [%v]", devices[20].Data["info"])
+	}
+	if devices[20].Data["oid"] != ".1.3.6.1.2.1.33.1.3.3.1.5.2" {
+		t.Fatalf("Expected oid == [.1.3.6.1.2.1.33.1.3.3.1.5.2], got [%v]", devices[20].Data["oid"])
+	}
 
 	t.Logf("TestUpsMib end")
 }
